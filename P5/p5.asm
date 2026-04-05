f10feb54c31aeff5a35278b69f850249dafc7fae
salomepino-code <salome.pino@uabc.edu.mx>
2026-04-05 21:51:34 +0000

    inciso a

--- P5/p5.asm
+++ P5/p5.asm
@@ -1,6 +1,9 @@
 %include "../LIB/pc_io.inc"  	; incluir declaraciones de procedimiento externos
 								; que se encuentran en la biblioteca libpc_io.a
 
+section	.data                                       
+msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 ; se pueden poner variables arriba
+
 section	.text
 	global _start       ;referencia para inicio de programa
 	
@@ -8,10 +11,13 @@ _start:
 	mov edx, msg		; edx = dirección de la cadena msg
 	call puts			; imprime cadena msg terminada en valor nulo (0)
 
+	mov byte[msg], 'Z'
+
+	mov edx, msg
+	call puts
+
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
 
-section	.data
-msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
 
 
