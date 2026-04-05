931a9afeb1bfe72d26611fb05430760bf19b2438
salomepino-code <salome.pino@uabc.edu.mx>
2026-04-05 22:15:03 +0000

    inciso f

--- P5/p5.asm
+++ P5/p5.asm
@@ -34,7 +34,12 @@ _start:
 	mov edx, msg
 	call puts
 
+	mov ebx, msg
+	mov esi, 0
 	mov byte [ebx + esi*1 + 19], '%'
+
+	mov edx, msg
+	call puts
 
 
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
