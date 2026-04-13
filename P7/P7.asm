8183f1d172a465aea0c60d81f8857486593b7866
salomepino-code <salome.pino@uabc.edu.mx>
2026-04-13 23:57:05 +0000

    a

--- P7/P7.asm
+++ P7/P7.asm
@@ -1,2 +1,31 @@
+%include "../LIB/pc_iox.inc"
 
-;
+section .data
+    N dw 0
+
+section .text
+    global _start
+
+_start:
+
+;inciso a
+mov eax, 0x22446688
+ROL eax, 1
+
+extern pBin_dw
+mov al, 10
+call putchar
+
+;inciso b
+mov cx, 0x3F48
+sal cx, 
+
+extern pBin_dw
+mov al, 10
+call putchar
+
+
+
+; fin del programa
+    mov eax, 1
+    int 0x80
