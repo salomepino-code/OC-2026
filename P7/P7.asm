b7a08ca2de3dd10a12ea9aebea02dd1100b64906
salomepino-code <salome.pino@uabc.edu.mx>
2026-04-14 00:31:16 +0000

    incluir p7

--- P7/P7.asm
+++ P7/P7.asm
@@ -1,4 +1,4 @@
-%include "../LIB/pc_iox.inc"
+%include "../LIB/pbin.o"
 
 section .data
     N dw 0
@@ -20,12 +20,39 @@ call putchar
 mov cx, 0x3F48
 sal cx, 
 
-extern pBin_dw
+extern pBin_w
+mov al, 10
+call putchar
+
+
+;inciso c
+esi, 0x20D685F3
+XOR esi, 1
+
+extern pBin_w
+mov al, 10
+call putchar
+
+;inciso d
+push esi
+
+extern pBin_w
+mov al, 10
+call putchar
+
+;inciso e
+mov ch, 0xA7
+
+extern pBin_w
 mov al, 10
 call putchar
 
 
 
+
+
+
 ; fin del programa
     mov eax, 1
+    mov ebx, 0
     int 0x80
