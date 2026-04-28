0d42583383f115bea81c180c75e807aa241f478b
salomepino-code <salome.pino@uabc.edu.mx>
2026-04-28 00:24:16 +0000

    inciso a

--- P9/P9.asm
+++ P9/P9.asm
@@ -1,5 +1,41 @@
 %include "../LIB/pc_io.inc"
 
-section.data
+section .data
+N, equ 5
+msg db "numero no valido", 0
 
-section.bss
+section .bss
+
+section .text
+
+;INCISO A 
+mov ecx, 0
+mov ebx, 0
+
+.Leer:
+call getche
+cmp al, '0'
+jb error
+cmp al '9'
+ja error
+
+sub al, '0'
+movzx ebx, al
+add ecx, 1
+cmp al, N 
+ja fin
+loop .leer
+
+;INCISO B 
+
+
+error:
+mov edx, msg
+call puts
+jmp fin
+
+fin:
+mov eax, 1
+mov ebx, 0
+int 0x80
+
