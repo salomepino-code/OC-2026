376285c3799f2d0ec64748865a61846c0605b19c
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-03 07:27:13 +0000

    ejericio2

--- Ejercicios/EjerciciosEx2.asm
+++ Ejercicios/EjerciciosEx2.asm
@@ -12,6 +12,13 @@ _start:
 mov ebx, cadena ; cadena terminada en cero ya definida en .data
 call PrintStr
 
+mov ebx, cadena
+call InvertirStr 
+
+mov ebx, cadena         ;  se invierte cadena directamente en su zona de memoria
+call PrintStr            ;  se presenta en pantalla odnuM aloH
+
+
 mov eax, 1
 mov ebx, 0
 int 0x80
@@ -27,5 +34,35 @@ inc ebx
 jmp loop
 
 fin:
+ret
+
+InvertirStr:
+
+mov esi, ebx
+mov edi, ebx
+
+.buscarFin:
+cmp byte [edi], 0
+jz .encontrado
+
+inc edi
+jmp .buscarFin
+
+.encontrado:
+dec edi
+
+.intercambiar:
+cmp esi, edi
+jae .fin
+
+mov al, [esi]
+mov dl, [edi]
+mov [esi], dl
+mov [edi], al
+inc esi
+dec edi
+jmp .intercambiar
+
+.fin:
 ret
 
