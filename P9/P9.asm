4f895d202bee9bcfaf611b3953e335388225be6b
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-03 00:02:08 +0000

    inciso d

--- P9/P9.asm
+++ P9/P9.asm
@@ -2,60 +2,91 @@
 %include "../LIB/pc_iox.inc"
 
 section .data
-N equ 10
+N equ 5
 msg db "numero no valido", 0
 
 section .bss
-vector resb 10
+vector1 resb 10
+vector2 resb 10
 
 section .text
 global _start
 
 _start:
 
-;INCISO A 
-mov ecx, 0
-mov ebx, vector
+;INCISO D (main)
 
-leer:
+mov ebx, vector1
+call leerVector
+
+mov ebx, vector2
+call leerVector
+
+mov ebx, vector1
+mov edx, vector2
+call sumar
+
+mov ebx, vector1
+call mostrar
+
+fin:
+mov eax, 1
+mov ebx, 0
+int 0x80
+
+;INCISO A 
+leerVector:
+mov ecx, 0
+
+.leer:
 call getche
 cmp al, '0'
-jb error
+jb .leer
 cmp al, '9'
-ja error
+ja .leer
 
 mov [ebx], al
 inc ebx
 inc ecx
 cmp ecx, N 
-jb leer
-jmp incisoB
+jb .leer
 
-incisoB:
+ret 
+
+;INCIDO B
+mostrar:
 
 mov ecx, 0
-mov ebx, vector 
 
-mostrar:
+.mostrar:
 mov al, [ebx]
 call pHex_b
-mov al,10	; cambio de linea
+mov al,10	
 call putchar
 inc ebx
 inc ecx
 cmp ecx, N 
-jb mostrar
-jmp fin
+jb .mostrar
 
-;MENSAJE
-error:
-mov edx, msg
-call puts
-jmp leer
+ret
 
-;SALIR 
-fin:
-mov eax, 1
-mov ebx, 0
-int 0x80
+;INCIDO C
+sumar:
+
+mov ecx, 0
+
+.sumar:
+mov al, [ebx]
+mov dl, [edx]
+add al, dl
+mov [ebx], al
+inc ebx
+inc edx
+inc ecx
+cmp ecx, N 
+jb .sumar
+
+ret
+
+
 
