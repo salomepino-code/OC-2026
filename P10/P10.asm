89bf137d4d38ce2f4fda107ef9cf4a4d4e4f7e0c
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-12 20:11:17 +0000

    practica 10

--- P10/P10.asm
+++ P10/P10.asm
@@ -1,12 +1,49 @@
+section .text
+global suma
+global mystrlen
+global getBit
 
-section .data
-section .bss
-section .test
 
-sumar: 
-push ebp 
-mov ebp, esp
-mov eax, [ebo + 0]
-add eax, [ebp + 12]
-pop ebp
-ret
+; int suma(int a, int b)
+
+suma:
+    push ebp
+    mov ebp, esp
+    mov eax, [ebp + 8]
+    add eax, [ebp + 12]
+    pop ebp
+    ret
+
+
+; int mystrlen(char *str)
+
+mystrlen:
+    push ebp
+    mov ebp, esp
+    mov ecx, [ebp + 8]
+    xor eax, eax
+
+.loop:
+    cmp byte [ecx + eax], 0
+    je  .done
+    inc eax
+    jmp .loop
+
+.done:
+    pop ebp
+    ret
+
+
+; int getBit(int value, int numbit)
+
+getBit:
+    push ebp
+    mov ebp, esp
+    mov eax, [ebp + 8]
+    mov ecx, [ebp + 12]
+    shr eax, cl
+    and eax, 1
+    pop ebp
+    ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
--- P10/programa.c
+++ P10/programa.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 
-extern suma(int a, int b);
+extern int suma(int a, int b);
+extern int mystrlen(char *str);
+extern int getBit(int value, int numbit);
 
 int main(){
-    print("suma = %d \n", suma(2,5));
+
+    printf("=== suma ===\n");
+    printf("suma = %d \n", suma(2,5));
+
+    printf("\n=== mystrlen ===\n");
+    printf("mystrlen(\"hola\")           = %d\n", mystrlen("hola"));
+
+    printf("\n=== getBit ===\n");
+    // 0b1010 = 10
+    printf("getBit(10, 0) = %d  (esperado 0)\n", getBit(10, 0));
     return 0;
 }
