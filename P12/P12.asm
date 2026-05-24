2c214029c2852fd2f05217ed97be8fbfbe5458d9
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-24 21:15:18 +0000

    a

--- P12/P12.asm
+++ P12/P12.asm
@@ -1,1 +1,18 @@
-;
+section .text
+global strlen
+global mystrlen
+global getBit
+
+streln:
+mov ecx, 0
+mov ebx, eax
+
+loop:
+mov eax, [ebx+1]
+inc ecx
+jz mostrar
+jnz loop
+
+mostrar:
+mov eax, ecx
+ret
