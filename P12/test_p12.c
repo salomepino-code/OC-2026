8516cb6745debab43eba3a0c2c638f2ad1fd38e4
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-25 08:09:00 +0000

    .

---
+++ P12/test_p12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+extern int strlen (char *str);
+extern void str_mid( char *str_out, char *str1_in, int start, int end);
+extern int clrBit( int value, int nbit) ;
+
+int main(){
+
+    printf("=== Inciso 1 ===\n");
+    printf("longitud = %d \n", strlen("hola"));
+
+    printf("\n=== Inciso 2 ===\n");
+    str_mid("Hola Mundo", 5, 10));
+
+    printf("\n=== Inciso 3 ===\n");
+    printf(" valor = %d  ", clrBit(0xFF, 3));
+    return 0;
+}
