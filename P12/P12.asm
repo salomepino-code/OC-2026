83871051c7b135850b08c79a27b52a5b499649c2
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-25 08:08:51 +0000

    cambio nombre

--- P12/P12.asm
+++ P12/P12.asm
@@ -1,18 +1,91 @@
 section .text
 global strlen
-global mystrlen
-global getBit
+global copiar
+global clrBit
 
-streln:
+;=========
+;INCISO 1 
+;=========
+
+strlen:
+push ebp
+mov ebp, esp
+
+mov edi, [ebp + 8]
 mov ecx, 0
-mov ebx, eax
 
-loop:
-mov eax, [ebx+1]
+.loop:
+cmp byte[edi], 0
+jz .fin
 inc ecx
-jz mostrar
-jnz loop
+inc edi
+jmp .loop
 
-mostrar:
+.fin:
 mov eax, ecx
+pop ebp
 ret
+
+;=========
+;INCISO 2 
+;=========
+
+copiar:
+push ebp
+mov ebp, esp
+
+push esi
+push edi
+push ebx
+
+mov edx, [ebp + 16]
+mov esi, [ebp + 12]
+add esi, edx
+mov edi, [ebp + 8]
+mov ebx, edx
+mov ecx, [ebp + 20]
+
+.loop
+cmp ebx, ecx
+jae .fin
+mov al, [esi]
+mov [edi], al
+inc esi
+inc edi
+inc ebx 
+jmp .loop
+
+.fin:
+mov byte[edi], 0
+
+pop ebx
+pop edi
+pop esi
+pop ebp 
+ret
+
+;=========
+;INCISO 3 
+;=========
+
+clrBit:
+push ebp
+mov ebp, esp
+
+push ecx
+push edx
+
+mov edx, [ebp + 8]
+mov al, [ebp + 12]
+
+mov eax, 1
+shl eax, cl
+not eax
+and edx, eax
+
+pop edx
+pop ecx
+
+pop ebd
+ret
+
