b420bb8ccee87d31e09f4babd8121fb54d7853ea
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-31 02:49:19 +0000

    swap

--- repasos/SwapNibble.asm
+++ repasos/SwapNibble.asm
@@ -1,38 +1,36 @@
-
+section .text
+global SwapNibble
 
 SwapNibble:
 push ebp
 mov ebp, esp
-
-mov eax, [ebp+8]
-mov cl, [ebp+12]
+mov eax, [esp+8]
+mov cl, [esp+12]
 call getNibble
-
-mov cl, [ebp+16]
+mov cl, [esp+16]
 call putNibble
-
 mov edx, eax
-mov eax, [ebp+8]
-mov cl, [ebp+16]
+mov eax, [esp+8]
+mov cl, [esp+16]
 call getNibble
-
 mov eax, edx
-mov cl, [ebp+12]
+mov cl, [esp+12]
 call putNibble
-
 pop ebp
 ret
 
 getNibble:
 shl cl, 2
 ror eax, cl
-mov ch, al
+mov dl, al
 and al, 0x0f
 rol eax, cl
+ret
 
 putNibble:
 shl cl, 2
 ror eax, cl
-and al, 0x0f
-or al, ch
+and al, 0xf0
+or al, dl
 rol eax, cl
+ret
