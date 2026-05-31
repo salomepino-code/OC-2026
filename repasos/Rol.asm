a6446a798ba6a4a2d127673d3da4aa9f4ab50624
salomepino-code <salome.pino@uabc.edu.mx>
2026-05-31 03:54:06 +0000

    3h

---
+++ repasos/Rol.asm
@@ -0,0 +1,27 @@
+section .text
+global rol
+
+rol:
+push ebp
+mov ebp, esp
+push ebx
+mov edx, [ebp+8]
+mov ebx, [ebp+12]
+mov ecx, [ebp+16]
+
+.reducir:
+cmp ecx, ebx
+jb .repetir
+sub ecx, ebx
+jmp .reducir
+
+.repetir:
+shl edx, 1
+adc edx, 0
+dec ecx
+cmp ecx, 0
+jnz .repetir
+
+pop ebx
+pop ebp
+ret
