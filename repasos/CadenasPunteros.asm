3467173fb5b9a3831bec21fb29dd95802555ba8e
salomepino-code <salome.pino@uabc.edu.mx>
2026-06-02 01:49:20 +0000

    1h

--- repasos/CadenasPunteros.asm
+++ repasos/CadenasPunteros.asm
@@ -1,4 +1,51 @@
- PrintStr
- InvertirStr 
- PrintStrX
- strncpy
+PrintStr:
+.loop:
+cmp byte[ebx], 0
+jz .fin
+mov al, byte[ebx]
+call putchar
+inc ebx
+jmp .loop
+
+.fin:
+ret
+
+;===================
+
+InvertirStr:
+mov esi, ebx
+mov edi, ebx
+
+.encontrarFin:
+cmp byte[edi], 0
+jz .encontrado
+inc edi
+jmp .encontrarFin
+
+.encontrado:
+dec edi
+
+.intercambiar:
+cmp esi, edi
+jae .fin
+mov al, byte[edi]
+mov ah, byte[esi]
+mov byte[esi], ah
+mov byte[esi], al
+inc esi
+dec edi
+jmp .intercambiar
+
+.fin:
+ret
+
+;====================
+
+PrintStrX:
+push ebp
+mov ebp, esp
+
+pop ebp
+ret
+
+strncpy:
