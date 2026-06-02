2d9b23fd7980670ae25fc61b73090afba99fb12a
salomepino-code <salome.pino@uabc.edu.mx>
2026-06-02 06:56:43 +0000

    3h

--- repasos/CadenasPunteros.asm
+++ repasos/CadenasPunteros.asm
@@ -44,8 +44,80 @@ ret
 PrintStrX:
 push ebp
 mov ebp, esp
+push ebx
+push edi
 
+mov ebx, [ebp+8]
+mov cl, [ebp+12]
+
+mov edi, ebx
+cmp cl, 1
+jz .normal
+cmp cl, 0
+jz .buscarFin
+
+.normal:
+mov al, byte[ebx]
+call putchar
+inc ebx
+cmp byte[ebx], 0
+jz .fin
+jmp .normal
+
+.buscarFin:
+cmp byte[edi], 0
+jz .inverso
+inc edi
+jmp .buscarFin
+
+.inverso:
+dec edi
+mov al, byte[edi]
+call putchar
+cmp edi, ebx
+je .fin
+jmp .inverso
+
+.fin:
+pop edi
+pop ebx
 pop ebp
 ret
 
+;====================
 strncpy:
+push ebp
+mov ebp, esp
+push ebx
+push esi
+
+mov ebx, [ebp+8]
+mov esi, [ebp+12]
+mov ecx, [ebp+16]
+mov edx, 0
+
+.loop:
+cmp byte[esi], 0
+jz .acomodar
+mov al, byte[esi]
+mov byte[ebx], al
+inc ebx
+inc esi
+inc edx
+cmp edx, ecx
+jz .fin
+jmp .loop
+
+.acomodar:
+cmp edx, ecx
+jz .fin
+mov byte[ebx], 0
+inc ebx
+inc edx
+jmp .acomodar
+
+.fin:
+pop esi
+pop ebx
+pop ebp
+ret
