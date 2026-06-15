0a1e380da673d06cf968654504499b4a056f9ad8
salomepino-code <salome.pino@uabc.edu.mx>
2026-06-15 20:19:28 +0000

    4

--- repasos/aplicarClave.asm
+++ repasos/aplicarClave.asm
@@ -1,2 +1,19 @@
 uint32_t aplicarClave(uint32_t dato, uint32_t clave);
 Recibe un dato y una clave. Retorna dato XOR clave. Si el resultado es 0, retorna la clave original en su lugar.
+
+aplicarClave:
+push ebp
+mov ebp, esp
+mov eax, [ebp+8]
+mov edx, [ebp+12]
+xor eax, edx
+cmp eax, 0
+jz .retornaClave
+.fin
+
+.retornaClave:
+mov eax, edx
+
+.fin:
+pop ebp
+ret
--- repasos/intercambiar.asm
+++ repasos/intercambiar.asm
@@ -1,2 +1,15 @@
 void intercambiar(uint32_t *a, uint32_t *b);
 Intercambia los valores en las dos direcciones sin usar una variable temporal en memoria, solo registros.
+
+intercambiar:
+push ebp
+mov ebp, esp
+push ebx
+mov ebx, [ebp+8]
+mov eax, [ebx]
+mov ecx, [ebp+12]
+mov edx, [ecx]
+mov [ebx], edx
+mov [ecx], eax
+pop ebp
+ret
